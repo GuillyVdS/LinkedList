96d863f65e845fa12bb074d8b9dd9c9a02315a7a
Guillaume Van de Sype <guillaume.vds.97@gmail.com>
2021-07-20 23:39:14 +0100

    moving fgets input functionality to a separate function. Cleaning up comments

--- LinkedList.c
+++ LinkedList.c
@@ -14,12 +14,23 @@ typedef struct node {
   struct node * next;
 }ListNode;
 
-
-char status = 's';
-
+//creating prototypes for each function
+void getCharInput(char * inputValue);
 void pushToList(ListNode ** head, char * newvalue);
 void printItemsInList(ListNode * head);
 void removeItemFromList(ListNode ** head, char * valueToRemove);
+
+
+/*function used to retrieve character input from the command line. User input
+will be stored at the address of inputValue */
+void getCharInput(char * inputValue){
+  if( fgets(inputValue, 100, stdin) != NULL)
+  {
+    /*goes to the index of the newline character in inputValue and replaces it
+    with a NULL character*/
+    inputValue[strcspn ( inputValue, "\n" )] = 0;
+  }
+}
 
 void pushToList(ListNode ** head_ptr, char * newvalue) {
 
@@ -41,15 +52,15 @@ void pushToList(ListNode ** head_ptr, char * newvalue) {
 
         //sets pointer to refer to new node in the list and sets value of new node
         currentNode->next = (ListNode *) malloc(sizeof(ListNode));
-        currentNode->next->value = (char *)malloc(strlen(newvalue) + 1) ;
+        currentNode->next->value = (char *)malloc(strlen(newvalue) + 1);
         strcpy(currentNode->next->value, newvalue);
         //currentNode->next->value = newvalue;
         currentNode->next->next = NULL;
       }
   }
 
-  //iterates over each value in the list and checks if pointer is NULL, if it is not NULL it prints the value and moves the cursor to the next node
-  void printItemsInList(ListNode * head){
+//iterates over each value in the list and checks if pointer is NULL, if it is not NULL it prints the value and moves the cursor to the next node
+void printItemsInList(ListNode * head){
   ListNode * currentNode = head;
 
   while(currentNode != NULL){
@@ -59,19 +70,15 @@ void pushToList(ListNode ** head_ptr, char * newvalue) {
   }
 }
 
-  //finds specified item in list and removes it
-  void removeItemFromList(ListNode ** head_ptr, char * valueToRemove){
-    //printf("%s\n", valueToRemove );
-    ListNode * previousNode = NULL;
-    //sets a cursor to iterate over nodes, starting with head
-    ListNode * currentNode = *head_ptr;
+//finds specified item in list and removes it
+void removeItemFromList(ListNode ** head_ptr, char * valueToRemove){
+  //printf("%s\n", valueToRemove );
+  ListNode * previousNode = NULL;
+  //sets a cursor to iterate over nodes, starting with head
+  ListNode * currentNode = *head_ptr;
 
-    //printf("testOne\n" );
-    //printf("%s\n", head->value );
-    //iterates over each node as long as it's value is not NULL, checks if the value matches the value passed into this function.
-    int i = 0;
-
-
+  //iterates over each node as long as it's value is not NULL, checks if the value matches the value passed into this function.
+  //int i = 0;
 
     while(currentNode != NULL){
 
@@ -97,72 +104,41 @@ void pushToList(ListNode ** head_ptr, char * newvalue) {
     }
   }
 
-int main(){
-  char status = 'i';
-  ListNode * head = NULL;
-  char * inputValue;
-/*
-  ListNode * head = (ListNode *)malloc(sizeof(ListNode));
-  if(head == NULL){
-    return 1;
-  }
-  head->value = (char *)malloc(sizeof(10)) ;
-  memcpy(head->value, "MEMTEST", strlen("MEMTEST"));
-  head->next = NULL;*/
 
+
+
+int main(){
+  char status = 0;
+  ListNode * head = NULL;
+
+  char inputValuetest [100] = {0};
 
   while(status != 'e'){
-    char status = 'i';
-    printf("Please choose one of the following: (P)rint current list, (A)dd item to list, (R)emove item from list, (E)xit.\n");
-    scanf(" %c" , &status);
-    char * inputValue;
-    switch(status)
+    printf("Please choose one of the following: (P)rint current list, (A)dd item to list, (R)emove item from list, (E)xit: ");
+    fgets(inputValuetest, 100, stdin);
+    char inputValue [100] = {0};
+    switch(inputValuetest[0])
     {
       case 'P':
         printf("Printing current contents:\n");
         printItemsInList(head);
         break;
       case 'A':
-        printf("Please enter new item:\n");
-        scanf("%s", inputValue);
+        printf("Please enter new item: ");
+        getCharInput(inputValue);
         pushToList(&head, inputValue);
         break;
       case 'R':
-        //char * valueToRemove;
-        printf("Please enter item to remove:\n");
-        scanf("%s", inputValue);
-        printf("%s\n", inputValue );
+        printf("Please enter item to remove: ");
+        getCharInput(inputValue);
         removeItemFromList(&head, inputValue);
         break;
-      case 'E': //closes program
+      case 'E': //closes program,
         printf("Terminating Program...\n");
         return 0;
+      default:
+        printf("An error has occured.\n");
     }
+    memset(inputValuetest,0,100);
   }
-
-
-
-/*
-
-  pushToList(&head, "WOOF\0"); //& used to pass memory address of head into the function so the function can alter the variable at the location in the parent scope
-  pushToList(&head, "WOOF\0"); //will need to fix second value, currently program will delete first value if it matches strcmp, if second value matches it will again delete said value and thus delete the head value.
-  pushToList(&head, "BARK\0");
-  pushToList(&head, "BARK\0");
-  pushToList(&head, "WOOF\0");
-  pushToList(&head, "WOOF\0");
-  pushToList(&head, "WOOF\0");
-  pushToList(&head, "BARK\0");
-  pushToList(&head, "BARK\0");
-  printf("----\n");
-  printItemsInList(head);
-  removeItemFromList(&head, "WOOF");
-  printf("----\n");
-  //printf("%s\n", head->value);
-  printItemsInList(head);
-  pushToList(&head, "MAOW\0");
-  pushToList(&head, "MAOW\0");
-  printf("----\n");
-  //removeItemFromList(&head, "BARK");
-  printItemsInList(head);
-*/
 }
